822c2c5608cb1e57aaa28c40b7356d4782c2ce56
Aysen Aydin <aysen904@gmail.com>
2024-10-15 16:05:41 +0300

    create  slider and integration

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store";
-
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
+
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
