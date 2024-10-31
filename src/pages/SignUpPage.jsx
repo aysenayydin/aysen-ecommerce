7b7bc5441e4e52445089c0ff7d3821e7896b6848
Aysen Aydin <aysen904@gmail.com>
2024-10-31 16:44:11 +0300

    sign up page complete

---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,38 @@
+import SignUp from "@/components/shared/SignUp";
+import { getRoles } from "@/lib/apiService";
+import React, { useEffect, useState } from "react";
+
+function SignUpPage() {
+  const [roles, setRoles] = useState([
+    {
+      id: 1,
+      name: "Yönetici",
+      code: "admin",
+    },
+    {
+      id: 2,
+      name: "Mağaza",
+      code: "store",
+    },
+    {
+      id: 3,
+      name: "Müşteri",
+      code: "customer",
+    },
+  ]);
+  // useEffect(() => {
+  //   getRoles()
+  //     .then((response) => {
+  //       setRoles(response.data);
+  //       console.log("data", response.data);
+  //     })
+  //     .catch((error) => console.error(error));
+  // }, []);
+  return (
+    <div>
+      <SignUp roles={roles} />
+    </div>
+  );
+}
+
+export default SignUpPage;
