4a9223cd3d4548e212239c38c0ac6f3ca4cac914
Aysen Aydin <aysen904@gmail.com>
2024-10-31 16:54:52 +0300

    get roles with axios

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -4,31 +4,15 @@ import { getRoles } from "@/lib/apiService";
 import React, { useEffect, useState } from "react";
 
 function SignUpPage() {
-  const [roles, setRoles] = useState([
-    {
-      id: 1,
-      name: "Yönetici",
-      code: "admin",
-    },
-    {
-      id: 2,
-      name: "Mağaza",
-      code: "store",
-    },
-    {
-      id: 3,
-      name: "Müşteri",
-      code: "customer",
-    },
-  ]);
-  // useEffect(() => {
-  //   getRoles()
-  //     .then((response) => {
-  //       setRoles(response.data);
-  //       console.log("data", response.data);
-  //     })
-  //     .catch((error) => console.error(error));
-  // }, []);
+  const [roles, setRoles] = useState([]);
+  useEffect(() => {
+    getRoles()
+      .then((response) => {
+        setRoles(response.data);
+        console.log("data", response.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
   return (
     <InnerContainer>
       <SignUp roles={roles} />
